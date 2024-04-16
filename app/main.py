916d09d8268d04ef15b6ff035de50c953d540865
Karolina Rojek-Sito <karolinarojek-sito@MacBook-Pro-Karolina.local>
2024-04-16 02:15:16 +0200

    update main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 def get_clean_data():
-    data = pd.read_csv("../data/data.csv")
+    data = pd.read_csv("data/data.csv")
 
     data = data.drop(['Unnamed: 32', 'id'], axis=1)
 
@@ -136,8 +136,8 @@ def get_radar_chart(input_data):
     return fig
 
 def add_predictions(input_data):
-    model = pickle.load(open("../file/model.pkl", 'rb'))
-    scaler = pickle.load(open("../file/scaler.pkl", 'rb')) 
+    model = pickle.load(open("file/model.pkl", 'rb'))
+    scaler = pickle.load(open("file/scaler.pkl", 'rb')) 
 
     input_array = np.array(list(input_data.values())).reshape(1,-1) 
 
